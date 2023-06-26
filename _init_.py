83a9e97a8e5189cbad0c458ceb4d6f1d4b9170e0
Zerensky <pure.evil97@mail.ru>
2023-06-26 17:15:47 +0600

    init

---
+++ _init_.py
@@ -0,0 +1,6 @@
+from make_files import make_files
+from two_files_in_one import two_files_in_one
+from fill_names import feel_numbers
+from name_gen import name_gen
+
+__all__ = ['make_files', 'two_files_in_one', 'feel_numbers', 'name_gen', 'group_rename']
